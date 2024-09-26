36a87bd41f4578b647c778a2c0b629abf91e33fb
Thinh <Thinh@DESKTOP-RS2T3UO>
2024-09-26 19:41:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/foodorder/constant/AboutUsConfig.java
@@ -0,0 +1,18 @@
+package com.example.foodorder.constant;
+
+public interface AboutUsConfig {
+    String ABOUT_US_TITLE = "Food Order";
+    String ABOUT_US_CONTENT = "Cửa hàng cung cấp dịch vụ đặt đồ ăn trực tuyến với thực đơn đa dạng, phục vụ nhanh chóng. " +
+            "Khách hàng dễ dàng đặt món qua ứng dụng và nhận đồ ăn tận nơi.";
+    String ABOUT_US_SLOGAN = "Highly committed to quality products & excellent services";
+    String ABOUT_US_WEBSITE_TITLE = "Website Food Order";
+
+    String PAGE_FACEBOOK = "";
+    String LINK_FACEBOOK = "https://www.facebook.com/";
+    String LINK_YOUTUBE = "https://www.youtube.com/";
+    String PHONE_NUMBER = "";
+    String GMAIL = "";
+    String SKYPE_ID = "";
+    String ZALO_LINK = "";
+    String WEBSITE = "";
+}
